b9d05f9d2dde92850832dda3a979351b687b64f6
Renujan <roshanrenujan20020426@gmail.com>
2025-09-04 09:43:32 +0530

    new_data

--- Items/urls.py
+++ Items/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('items/', ItemListView.as_view(), name='item-list'),
-    path('brands/', BrandListView.as_view(), name='brand-list'),
-    path('stocks/', StockListView.as_view(), name='stock-list'),
-    path('stocks/<int:pk>/', StockDetailView.as_view(), name='stock-detail'),
-    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('items/', ItemListView.as_view(), name='item-list'),#show all items
+    path('brands/', BrandListView.as_view(), name='brand-list'),#show all brands
+    path('stocks/', StockListView.as_view(), name='stock-list'),#`show all stocks
+    path('stocks/<int:pk>/', StockDetailView.as_view(), name='stock-detail'),#show, update stock by id
+    path('categories/', CategoryListView.as_view(), name='category-list'),#show all categories
 ]
