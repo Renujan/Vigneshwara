ab87c72eede221c89c3ec1ae22f84323000ab5a3
Renujan <roshanrenujan20020426@gmail.com>
2025-09-07 17:01:18 +0530

    db

--- Items/urls.py
+++ Items/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('items/', ItemListView.as_view(), name='item-list'),#show all items
+    path('items/', ItemListView.as_view(), name='item-list'),#show all items from db
     path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
     path('brands/', BrandListView.as_view(), name='brand-list'),#show all brands
     path('stocks/', StockListView.as_view(), name='stock-list'),#`show all stocks
